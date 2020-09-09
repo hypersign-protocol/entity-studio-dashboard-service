e00747c071270ef4d9da565beb5ae9d181d926b7
vishwas1 <vishwasanand.cse@gmail.com>
2020-09-09 06:43:47 +0530

    moved unwanted files to backup

--- client/src/router.js
+++ client/src/router.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import PKIIdLogin from './views/PKIIdLogin.vue'
-import AppDetails from './views/AppDetails.vue'
-import IssueCredential from './views/IssueCredential.vue'
+// import AppDetails from './views/AppDetails.vue'
+// import IssueCredential from './views/IssueCredential.vue'
 import Credential from './views/Credential.vue'
 import Presentation from './views/Presentation.vue'
 import Dashboard from './views/Dashboard.vue'
-import Profile from './views/Profile.vue'
+// import Profile from './views/Profile.vue'
 import fetch from 'node-fetch'
 import Schema from './views/Schema.vue'
 
@@ -36,14 +36,14 @@ const router =  new Router({
         requiresAuth: true
       } 
     },
-    {
-      path: '/studio/profile',
-      name: 'profile',
-      component: Profile,
-      meta: {
-        requiresAuth: true
-      } 
-    },
+    // {
+    //   path: '/studio/profile',
+    //   name: 'profile',
+    //   component: Profile,
+    //   meta: {
+    //     requiresAuth: true
+    //   } 
+    // },
     {
       path: '/studio/schema',
       name: 'schema',
@@ -52,14 +52,14 @@ const router =  new Router({
         requiresAuth: true
       } 
     },
-    {
-      path: '/studio/apps/:appId',
-      name: 'appdetails',
-      component: AppDetails,
-      meta: {
-        requiresAuth: true
-      } 
-    },
+    // {
+    //   path: '/studio/apps/:appId',
+    //   name: 'appdetails',
+    //   component: AppDetails,
+    //   meta: {
+    //     requiresAuth: true
+    //   } 
+    // },
     {
       path: '/studio/credential',
       name: 'credential',
@@ -76,14 +76,14 @@ const router =  new Router({
         requiresAuth: true
       } 
     },
-    {
-      path: '/studio/apps/:appId/issue',
-      name: 'issueCredential',
-      component: IssueCredential,
-      meta: {
-        requiresAuth: true
-      } 
-    }
+    // {
+    //   path: '/studio/apps/:appId/issue',
+    //   name: 'issueCredential',
+    //   component: IssueCredential,
+    //   meta: {
+    //     requiresAuth: true
+    //   } 
+    // }
   ]
 })
 
