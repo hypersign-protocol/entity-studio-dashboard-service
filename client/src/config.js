c47ffb270364cc8c6d465fb401156e5d3170a32c
Pratap <pratap@hypermine.in>
2022-08-26 16:47:24 +0530

    fix env

--- client/src/config.js
+++ client/src/config.js
@@ -1,20 +1,22 @@
 import EnvProvider from 'jvjr-docker-env'
+
 const hsdk = require('lds-sdk')
+console.log(process.env);
 const config = {
     studioServer: {
-        BASE_URL: EnvProvider.value('STUDIO_SERVER_BASE_URL'),
-        ACCPCT_CRED_EP: EnvProvider.value('ACCPCT_CRED_EP')|| "api/v1/credential/accepct",
-        SAVE_SCHEMA_EP: EnvProvider.value('STUDIO_SERVER_SAVE_SCHEMA')||"api/v1/schema/issue",
-        CRED_LIST_EP: EnvProvider.value('STUDIO_SERVER_CRED_LIST_EP') || "api/v1/credential/list",
-        SCHEMA_LIST_EP:EnvProvider.value('SCHEMA_LIST_EP')|| 'api/v1/schema/get',
+        BASE_URL: process.env.VUE_APP_STUDIO_SERVER_BASE_URL, // EnvProvider.value('STUDIO_SERVER_BASE_URL'),
+        ACCPCT_CRED_EP: process.env.VUE_APP_ACCPCT_CRED_EP|| "api/v1/credential/accepct",
+        SAVE_SCHEMA_EP: process.env.VUE_APP_STUDIO_SERVER_SAVE_SCHEMA ||"api/v1/schema/issue",
+        CRED_LIST_EP: process.env.VUE_APP_STUDIO_SERVER_CRED_LIST_EP || "api/v1/credential/list",
+        SCHEMA_LIST_EP: process.env.VUE_APP_SCHEMA_LIST_EP || 'api/v1/schema/get',
         CRED_ISSUE_EP: EnvProvider.value('STUDIO_SERVER_CRED_ISSUE_EP') || "api/v1/credential/issue",
         AUTH_CHALLENGE_EP: EnvProvider.value('STUDIO_SERVER_AUTH_CHALLENGE_EP') || "api/auth/challenge",
         AUTH_LOGIN_EP: EnvProvider.value('STUDIO_SERVER_AUTH_LOGIN_EP') || "api/auth/login"
     },
     nodeServer: {
-        BASE_URL: EnvProvider.value('NODE_SERVER_BASE_URL') ,
-        BASE_URL_REST: EnvProvider.value('NODE_SERVER_BASE_URL_REST') || 'https://jagrat.hypersign.id/node1/rest/',
-        SCHEMA_GET_REST:EnvProvider.value('SCHEMA_GET_EP_REST') || 'hypersign-protocol/hidnode/ssi/schema/',
+        BASE_URL: process.env.VUE_APP_NODE_SERVER_BASE_URL,
+        BASE_URL_REST: process.env.VUE_APP_NODE_SERVER_BASE_URL_REST || 'https://jagrat.hypersign.id/node1/rest/',
+        SCHEMA_GET_REST:process.env.VUE_APP_SCHEMA_GET_EP_REST || 'hypersign-protocol/hidnode/ssi/schema/',
         NETWORK_STATUS_EP: EnvProvider.value('NODE_SERVER_NETWORK_STATUS_EP') || "net_info",
         SCHEMA_LIST_EP: EnvProvider.value('NODE_SERVER_SCHEMA_LIST_EP') || "api/schema/list",
         SCHEMA_GET_EP: EnvProvider.value('NODE_SERVER_SCHEMA_GET_EP') || "api/schema/get",
@@ -32,9 +34,9 @@ const config = {
     },
 
 }
-const websocketUrl=  EnvProvider.value('STUDIO_SERVER_BASE_WS') ||  'ws://localhost:9000' // 'wss://stage.hypermine.in/studioserverws/'
+const websocketUrl= process.env.VUE_APP_STUDIO_SERVER_BASE_WS
  //const webWalletAddress="https://wallet-stage.hypersign.id"
-const webWalletAddress="http://localhost:4999/chrome/popup/popup#"
+const webWalletAddress=process.env.VUE_APP_WEB_WALLET_ADDRESS;//"http://localhost:4999/chrome/popup/popup#"
 const options = { nodeUrl: config.nodeServer.BASE_URL, didScheme: "did:hs" }
 const hypersignSDK = {
     did: hsdk.did(options),
