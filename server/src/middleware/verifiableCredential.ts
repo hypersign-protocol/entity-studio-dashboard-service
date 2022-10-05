e011fc6010b0beac45b05ba4ae3e1ea0e914f3b8
varsha <varshakumari370@gmail.com>
2022-10-05 15:15:00 +0530

    updated credential module

---
+++ server/src/middleware/verifiableCredential.ts
@@ -0,0 +1,14 @@
+import { body, param } from 'express-validator';
+
+export const checkIfCredentialPramExists =[
+    param('orgDid').trim().exists({checkFalsy: true }).withMessage(' orgDid can not be null or empty')
+]
+
+export const credentialSchemaBody = [
+    body('QR_DATA').exists({checkFalsy: true}).withMessage('QR_DATA should present in body'),
+    body('QR_DATA.data.issuerDid').trim().exists({ checkFalsy: true}).withMessage('issuerDid can not be null or empty.'),
+    body('QR_DATA.data.subjectDid').trim().exists({ checkFalsy: true}).withMessage('subjectDid can not be null or empty.'),
+    body('QR_DATA.data.schemaId').trim().exists({ checkFalsy: true}).withMessage('schemaId can not be null or empty.'),
+    body('QR_DATA.data.orgDid').trim().exists({ checkFalsy: true}).withMessage("orgDid can not be null or empty.")
+
+]
