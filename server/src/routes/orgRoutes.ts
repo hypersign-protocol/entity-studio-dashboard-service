7d2142c319fe524b6bd049b52c0e1d43b86a4588
varsha <varshakumari370@gmail.com>
2022-10-05 08:33:24 +0530

    changed api end point

--- server/src/routes/orgRoutes.ts
+++ server/src/routes/orgRoutes.ts
@@ -4,11 +4,11 @@ import { CreateOrg, deleteOrg, GetOrgByDid, GetOrgById, GetOrgByIdSSE, GetOrgsBy
 
 export const orgRoutes = (hypersign) => {
     const router = Router();
-    router.post('/create', hypersign.authorize.bind(hypersign),CreateOrg);
+    router.post('/', hypersign.authorize.bind(hypersign),CreateOrg);
 
     router.post('/status/:id', setOrgStatus)
     router.put('/update', hypersign.authorize.bind(hypersign),updateOrg)
-    router.get('/all', hypersign.authorize.bind(hypersign), GetOrgsByUserDid)
+    router.get('/', hypersign.authorize.bind(hypersign), GetOrgsByUserDid)
     router.get('/sse/:id',GetOrgByIdSSE)
     router.get('/get/:id', hypersign.authorize.bind(hypersign),GetOrgById)
     router.get('/:orgDid', hypersign.authorize.bind(hypersign),GetOrgByDid)
