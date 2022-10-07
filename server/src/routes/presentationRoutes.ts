c48ddc20e977db986b4484339123c2e322305d9f
raj6939 <raj.v.patil108@gmail.com>
2022-10-07 11:44:32 +0530

    conflic resolve commiting

--- server/src/routes/presentationRoutes.ts
+++ server/src/routes/presentationRoutes.ts
@@ -1,20 +1,34 @@
-import { Router } from 'express'
-import { presentationTempalate, presentationTempalateAll, presentationTempalateById } from '../controllers/presentationController';
-import { getChallenge , verify} from '../controllers/presentationRequestController';
+import { Router } from 'express';
+import {
+  presentationTempalate,
+  verifyPresentation,
+  presentationTempalateAll,
+  presentationTempalateById,
+} from '../controllers/presentationController';
+import { getChallenge, verify } from '../controllers/pController';
+import { presentationSchemaBody, presentationSchemaParams } from '../middleware/presentation';
+import { validateRequestSchema } from '../middleware/validateRequestSchema';
 
 export const presentationRoute = (hypersign) => {
-    const router = Router()
+  const router = Router();
 
-    // PresentationRequest
-    router.get('/request/:presentationTemplateId', getChallenge)
-    router.post('/request/verify', verify)
-    
-    // PresentationTemplate
-    router.post('/template', hypersign.authorize.bind(hypersign), presentationTempalate)
-    router.get('/template/:id', hypersign.authorize.bind(hypersign), presentationTempalateById)
-    router.get('/template/org/:orgDid', hypersign.authorize.bind(hypersign), presentationTempalateAll)
-    return router;
-}
+  router.get('/request/:presentationTemplateId', getChallenge);
+  router.post('/request/verify', verify);
 
-
-
+  router.post(
+    '/template',
+    hypersign.authorize.bind(hypersign),
+    presentationSchemaBody,
+    validateRequestSchema,
+    presentationTempalate
+  );
+  router.get('/template/:id', hypersign.authorize.bind(hypersign), presentationTempalateById);
+  router.get(
+    '/template/org/:orgDid',
+    hypersign.authorize.bind(hypersign),
+    presentationSchemaParams,
+    validateRequestSchema,
+    presentationTempalateAll
+  );
+  return router;
+};
